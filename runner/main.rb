82a94638e096d5cb974e47b1da7861604d5c7eaa
Christopher Kleynhans <chris@kleynhans.ca>
2014-03-01 23:50:42 -0500

    Made the runner work.

--- runner/main.rb
+++ runner/main.rb
@@ -5,6 +5,8 @@ require 'hiredis'
 require 'redis'
 require 'tmpdir'
 require 'fileutils'
+require 'yaml'
+
 algorithm = ARGV[0]
 executable_location = ARGV[1]
 model_location = ARGV[2]
@@ -28,9 +30,62 @@ Dir.mktmpdir do |dir|
 
     puts "Copied required files."
 
+    # About to run, so we need to set up the redis data.
+    data = {
+        "start_time" => Time.now(),
+        "finished" => false,
+        "errored" => false,
+        "pareto_points_found" => 0,
+        "pareto_points" => [
+        ]
+    }
 
-    io = IO.popen(["java", "-jar", "./moolloy.jar", "./model.als", :err => :out])
-    io.each_line do |line|
-        puts line
+    redis.set(algorithm, data.to_yaml)
+    redis.publish(algorithm, nil)
+
+    IO.popen(["java", "-jar", "./moolloy.jar", "-s", "./model.als", :err => [:child, :out]]) do |io|
+        io.each_line do |line|
+            # Is this line saying we found a solution?
+            match = /.*Found a solution.*\[([^\]]*)\].*/.match(line)
+            if match
+                metrics = match[1].split(/,\s+/)
+                puts "Found solution with metrics [#{metrics.join(", ")}]."
+            end
+
+            # Is this line saying we found a pareto point?
+            match = /.*Found Pareto point.*\[([^\]]*)\].*/.match(line)
+            if match
+                metrics = match[1].split(/,\s+/)
+
+                # Add the pareto point to the model.
+                data["pareto_points"] << {
+                    "cost" => metrics[0],
+                    "performance" => metrics[1]
+                }
+                redis.set(algorithm, data.to_yaml)
+                redis.publish(algorithm, nil)
+
+                puts "Found pareto point with metrics [#{metrics.join(", ")}]."
+            end
+
+            # Is this line a logger error?
+            if line =~ /ERROR/i
+                puts line
+            end
+        end
     end
+
+    data["finished"] = true
+    data["finish_time"] = Time.now
+    
+    if $?.to_i != 0
+        puts "Moolloy failed."
+        data["errored"] = true
+    else
+        puts "Moolloy finished."
+        data["errored"] = false
+    end
+
+    redis.set(algorithm, data.to_yaml)
+    redis.publish(algorithm, nil)
 end
